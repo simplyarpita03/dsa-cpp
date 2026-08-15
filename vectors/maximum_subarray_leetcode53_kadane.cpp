0ad0c4611c9528524c7346b580b71cc416958369
Arpita Mandal <arpita03mandal@gmail.com>
2026-08-15 21:23:42 +0530

    Create maximum_subarray_leetcode53_kadane.cpp

---
+++ vectors/maximum_subarray_leetcode53_kadane.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currentSum=0;
+        int maxSum=INT_MIN;
+        for(int i=0;i<nums.size();i++) {
+            currentSum=currentSum+nums[i];
+            maxSum=max(maxSum,currentSum);
+            if(currentSum<0) {
+                currentSum=0;
+            }
+        }
+        return maxSum;
+    }
+};
